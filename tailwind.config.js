dfc523316b64f7c333009bfe52cfc08fba9713dc
Amanda Cardoso <amanda.cardoso@sislogica.com.br>
2024-12-11 17:19:04 -0300

    alteracoes card insert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",       
-    "./js/**/*.js",     
+    "./index.html",          
       "./style/**/*.css"      
   ],
   theme: {
     extend: {
       boxShadow: {        
         'custom-sw': '2px 2px 3px 2px rgba(0, 0, 0, 0.3)', // sombra mais forte
+      },
+      width:{
+        'custom-Cat':'135px',
       },
     },
   },
